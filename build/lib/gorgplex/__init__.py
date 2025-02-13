f7a95862b5a8b1f51c25cd476006993b64177410
cland29 <carl.lee.landskron@gmail.com>
2025-02-13 12:50:51 -0500

    Flesh out the initial library

---
+++ build/lib/gorgplex/__init__.py
@@ -0,0 +1,10 @@
+from .src.gorg import(
+    battle_viewer,
+    gorg, 
+    gorg_maker,
+    gorg_battler,
+)
+
+from .src.modifiers import(
+    modifier
+)
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="gorgplex",
+    version="0.0.10",
+    description="The gorgplex at your fingertips",
+    package_dir={"":"app"},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cland29/GorgPlex",
+    author="Carl Lee Landskron",
+    author_email="Carl.lee.landskron@gmail.com",
+    install_requires=["matplotlib >= 3.10.0"],
+    python_requires=">=3.12",
+)
